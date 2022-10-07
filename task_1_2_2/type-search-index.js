8440d30d7bdd015b5d8e90216d9bfbb796271e13
evangelionexpert <evangelionexpert@users.noreply.github.com>
2022-10-07 08:26:33 +0000

    Deploying to gh-pages from @ evangelionexpert/oop@9b0cee39ce1db846bfeaa6a2ba34f530ee487b17 🚀

---
+++ task_1_2_2/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.fit.smolyakov.tree","l":"DFSIterator"},{"p":"ru.nsu.fit.smolyakov.tree","l":"Node"},{"p":"ru.nsu.fit.smolyakov.tree","l":"Tree"}]
