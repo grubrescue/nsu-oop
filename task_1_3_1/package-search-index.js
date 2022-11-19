522b8a17ecb495fda0a06635b5695ce111c4bf3b
evangelionexpert <evangelionexpert@users.noreply.github.com>
2022-11-19 08:13:02 +0000

    Deploying to gh-pages from @ evangelionexpert/oop@2b4bce17b8ad4a43328dc7ea94967722fba6a0d9 🚀

--- task_1_3_1/package-search-index.js
+++ task_1_3_1/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"ru.nsu.fit.smolyakov.substring_finder"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.nsu.fit.smolyakov.substring_finder"}];updateSearchResults();
