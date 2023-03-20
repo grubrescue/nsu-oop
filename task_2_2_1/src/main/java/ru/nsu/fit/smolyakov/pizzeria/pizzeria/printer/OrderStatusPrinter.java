e396f801442d53c1ebdefaa9c55fbabfd514af4b
Pavel Smolyakov <pavel.smolyakov.83@gmail.com>
2023-03-20 12:26:56 +0700

    removed printer (as we're going to use JUL)

--- task_2_2_1/src/main/java/ru/nsu/fit/smolyakov/pizzeria/pizzeria/printer/OrderStatusPrinter.java
+++
@@ -1,7 +0,0 @@
-package ru.nsu.fit.smolyakov.pizzeria.pizzeria.printer;
-
-import ru.nsu.fit.smolyakov.pizzeria.pizzeria.entity.Order;
-
-public interface OrderStatusPrinter {
-    void print(Order order);
-}
