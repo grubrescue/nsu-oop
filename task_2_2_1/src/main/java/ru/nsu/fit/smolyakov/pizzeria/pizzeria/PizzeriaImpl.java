a82cfdd9e8ba4a2e8e4c2693935ec077e7e823a6
Pavel Smolyakov <pavel.smolyakov.83@gmail.com>
2023-03-20 13:50:45 +0700

    json mapping

--- task_2_2_1/src/main/java/ru/nsu/fit/smolyakov/pizzeria/pizzeria/PizzeriaImpl.java
+++ task_2_2_1/src/main/java/ru/nsu/fit/smolyakov/pizzeria/pizzeria/PizzeriaImpl.java
@@ -1,12 +1,20 @@
 package ru.nsu.fit.smolyakov.pizzeria.pizzeria;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.annotation.JsonAppend;
 import ru.nsu.fit.smolyakov.pizzeria.pizzeria.entity.Order;
 import ru.nsu.fit.smolyakov.pizzeria.pizzeria.entity.OrderDescription;
 import ru.nsu.fit.smolyakov.pizzeria.pizzeria.baker.Baker;
 import ru.nsu.fit.smolyakov.pizzeria.pizzeria.deliveryboy.DeliveryBoy;
 import ru.nsu.fit.smolyakov.pizzeria.pizzeria.orderqueue.OrderQueue;
+import ru.nsu.fit.smolyakov.pizzeria.pizzeria.orderqueue.OrderQueueImpl;
 import ru.nsu.fit.smolyakov.pizzeria.pizzeria.warehouse.Warehouse;
+import ru.nsu.fit.smolyakov.pizzeria.pizzeria.warehouse.WarehouseImpl;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -15,25 +23,44 @@ import java.util.logging.Logger;
 public class PizzeriaImpl implements PizzeriaOrderService,
                                      PizzeriaEmployeeService,
                                      PizzeriaOwnerService {
+    private final static ObjectMapper objectMapper = new ObjectMapper();
+
+    private final ExecutorService threadPool = Executors.newScheduledThreadPool(8);
+    private final Logger logger = Logger.getLogger("pizzeria");
+
     private final OrderQueue orderQueue;
     private final Warehouse warehouse;
     private final List<Baker> bakerList;
     private final List<DeliveryBoy> deliveryBoyList;
 
-    private final ExecutorService threadPool = Executors.newScheduledThreadPool(8);
-    private final Logger logger = Logger.getLogger("pizzeria");
-
     private boolean working = false;
     private int orderId = 0;
 
-    public PizzeriaImpl(OrderQueue orderQueue,
-                        Warehouse warehouse,
-                        List<Baker> bakerList,
-                        List<DeliveryBoy> deliveryBoyList) {
+    @JsonCreator
+    private PizzeriaImpl(OrderQueue orderQueue,
+                         Warehouse warehouse,
+                         List<Baker> bakerList,
+                         List<DeliveryBoy> deliveryBoyList) {
         this.orderQueue = orderQueue;
         this.warehouse = warehouse;
         this.bakerList = bakerList;
         this.deliveryBoyList = deliveryBoyList;
+    }
+
+    public PizzeriaImpl fromJson(InputStream stream) {
+        try {
+            return objectMapper.readValue(stream, PizzeriaImpl.class);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public PizzeriaImpl fromJson(String resourceName) {
+        return fromJson(this.getClass().getResourceAsStream(resourceName));
+    }
+
+    public PizzeriaImpl fromJson() {
+        return fromJson("PizzeriaConfiguration.json");
     }
 
     @Override
@@ -59,7 +86,7 @@ public class PizzeriaImpl implements PizzeriaOrderService,
     }
 
     @Override
-    public void stop() {
+    public synchronized void stop() {
         working = false;
     }
 }
