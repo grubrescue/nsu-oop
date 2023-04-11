d61cbdec940ad7c6d7d7e7391be6933549d656ee
Pavel Smolyakov <pavel.smolyakov.83@gmail.com>
2023-04-11 22:18:07 +0700

    Считаю, что коммит умер очень больно. Считаю, что он мучился в своей смерти. Жаль, конечно, этого добряка. Конечно, он mvp не написал. Но model, конечно, написал для mvp. Все равно жалко его. Хороший был ЗМЕЙКА

--- task_2_3_1/src/main/java/ru/nsu/fit/smolyakov/snake/model/Barrier.java
+++ task_2_3_1/src/main/java/ru/nsu/fit/smolyakov/snake/model/Barrier.java
@@ -2,7 +2,17 @@ package ru.nsu.fit.smolyakov.snake.model;
 
 import java.util.Set;
 
+/**
+ * Represents a barrier that is located on {@link GameField}.
+ * The snake cannot pass through the barrier.
+ */
 public record Barrier(Set<Point> barrierPoints){
+    /**
+     * Checks if the point is located on the barrier.
+     *
+     * @param point point
+     * @return {@code true} if the point is located on the barrier, false otherwise
+     */
     public boolean met(Point point) {
         return barrierPoints.contains(point);
     }
