f082389ff88101a927d411d8007b3089748fbc07
Pavel Smolyakov <pavel.smolyakov.83@gmail.com>
2023-04-17 23:19:25 +0700

    application supports both gui and console versions

--- task_2_3_1/src/main/java/ru/nsu/fit/smolyakov/snake/Application.java
+++ task_2_3_1/src/main/java/ru/nsu/fit/smolyakov/snake/Application.java
@@ -1,20 +1,33 @@
 package ru.nsu.fit.smolyakov.snake;
 
 import ru.nsu.fit.smolyakov.snake.executable.ConsoleSnakeGame;
+import ru.nsu.fit.smolyakov.snake.executable.JavaFxSnakeGame;
 
 import java.io.IOException;
 
 
 public class Application {
-    public static void main(String[] args) {
-//        var app = new JavaFxSnakeGame();
-//        app.execute();
-
+    private static void runJavaFxSnakeGame() {
+        var app = new JavaFxSnakeGame();
+        app.execute();
+    }
+    
+    private static void runConsoleSnakeGame() {
         var app = new ConsoleSnakeGame();
         try {
             app.execute();
         } catch (IOException e) {
             throw new RuntimeException(e);
+        }
+    }
+    
+    public static void main(String[] args) {
+        if (args.length == 0 || args[0].equals("--gui")) {
+            runJavaFxSnakeGame();
+        } else if (args[0].equals("--console")) {
+            runConsoleSnakeGame();
+        } else {
+            throw new RuntimeException("unknown argument: " + args[0]);
         }
     }
 }
