6c8806e9c6081b6fb2bbf0f9a7966a99191c6e07
Pavel Smolyakov <pavel.smolyakov.83@gmail.com>
2023-04-18 10:09:36 +0700

    properties docs

--- task_2_3_1/src/main/java/ru/nsu/fit/smolyakov/snakegame/properties/GameSpeed.java
+++ task_2_3_1/src/main/java/ru/nsu/fit/smolyakov/snakegame/properties/GameSpeed.java
@@ -1,17 +1,45 @@
 package ru.nsu.fit.smolyakov.snakegame.properties;
 
+/**
+ * Represents game speed.
+ */
 public enum GameSpeed {
+    /**
+     * The slowest game speed. The snake moves every 400 milliseconds.
+     */
     LEVEL_0(400),
+
+    /**
+     * The medium game speed. The snake moves every 200 milliseconds.
+     */
     LEVEL_1(200),
+
+    /**
+     * The fast game speed. The snake moves every 100 milliseconds.
+     */
     LEVEL_2(100),
+
+    /**
+     * The fastest game speed. The snake moves every 50 milliseconds.
+     */
     LEVEL_3(50);
 
     private final int frameDelayMillis;
 
+    /**
+     * Creates a new game speed.
+     *
+     * @param frameDelayMillis a delay between frames in milliseconds
+     */
     GameSpeed(int frameDelayMillis) {
         this.frameDelayMillis = frameDelayMillis;
     }
 
+    /**
+     * Returns a delay between frames in milliseconds.
+     *
+     * @return a delay between frames in milliseconds
+     */
     public int getFrameDelayMillis() {
         return frameDelayMillis;
     }
