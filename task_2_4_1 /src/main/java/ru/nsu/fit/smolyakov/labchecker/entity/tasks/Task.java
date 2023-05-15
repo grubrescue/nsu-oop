d3e65bf5dcc768d40a4c385da484baafd1c6c577
Pavel Smolyakov <pavel.smolyakov.83@gmail.com>
2023-05-15 13:33:48 +0700

    работаем

---
+++ task_2_4_1 /src/main/java/ru/nsu/fit/smolyakov/labchecker/entity/tasks/Task.java
@@ -0,0 +1,10 @@
+package ru.nsu.fit.smolyakov.labchecker.entity.tasks;
+
+public class Task {
+    private String name;
+    private String branch;
+    private String description = "no description provided";
+    private double points = 1.0;
+
+    private boolean runTests = true;
+}
