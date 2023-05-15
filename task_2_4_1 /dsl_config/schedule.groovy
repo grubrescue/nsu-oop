15550f48cbbb1be791e3da4eaa7518f69c6efe44
Pavel Smolyakov <pavel.smolyakov.83@gmail.com>
2023-05-15 22:22:32 +0700

    немного покрасивше оформил скрипты

--- task_2_4_1 /dsl_config/schedule.groovy
+++ task_2_4_1 /dsl_config/schedule.groovy
@@ -1,102 +1,102 @@
 assignments {
     // 1st semester
     assignment('task_1_1_1') {
-        softDeadlineAt('2022-09-12')
-        hardDeadlineAt('2022-09-26')
+        softDeadlineAt '2022-09-12'
+        hardDeadlineAt '2022-09-26'
     }
 
     assignment('task_1_2_1') {
-        softDeadlineAt('2022-09-27')
-        hardDeadlineAt('2022-10-10')
+        softDeadlineAt '2022-09-27'
+        hardDeadlineAt '2022-10-10'
     }
 
     assignment('task_1_2_2') {
-        softDeadlineAt('2022-10-10')
-        hardDeadlineAt('2022-10-24')
+        softDeadlineAt '2022-10-10'
+        hardDeadlineAt '2022-10-24'
     }
 
     assignment('task_1_2_3') {
-        softDeadlineAt('2022-10-24')
-        hardDeadlineAt('2022-11-07')
+        softDeadlineAt '2022-10-24'
+        hardDeadlineAt '2022-11-07'
     }
 
     assignment('task_1_3_1') {
-        softDeadlineAt('2022-11-07')
-        hardDeadlineAt('2022-11-21')
+        softDeadlineAt '2022-11-07'
+        hardDeadlineAt '2022-11-21'
     }
 
     assignment('task_1_4_1') {
-        softDeadlineAt('2022-11-21')
-        hardDeadlineAt('2022-12-06')
+        softDeadlineAt '2022-11-21'
+        hardDeadlineAt '2022-12-06'
     }
 
     assignment('task_1_5_1') {
-        softDeadlineAt('2022-12-06')
-        hardDeadlineAt('2022-12-20')
+        softDeadlineAt '2022-12-06'
+        hardDeadlineAt '2022-12-20'
     }
 
     assignment('task_1_5_2') {
-        softDeadlineAt('2022-12-20')
-        hardDeadlineAt('2023-12-27')
+        softDeadlineAt '2022-12-20'
+        hardDeadlineAt '2023-12-27'
     }
 
     // 2nd semester
     assignment('task_2_1_1') {
-        softDeadlineAt('2023-02-14')
-        hardDeadlineAt('2023-03-17')
+        softDeadlineAt '2023-02-14'
+        hardDeadlineAt '2023-03-17'
     }
 
     assignment('task_2_2_1') {
-        softDeadlineAt('2023-03-14')
-        hardDeadlineAt('2023-03-28')
+        softDeadlineAt '2023-03-14'
+        hardDeadlineAt '2023-03-28'
     }
 
     assignment('task_2_3_1') {
-        softDeadlineAt('2023-04-18')
-        hardDeadlineAt('2023-05-16')
+        softDeadlineAt '2023-04-18'
+        hardDeadlineAt '2023-05-16'
     }
 
     assignment('task_2_4_1') {
-        softDeadlineAt('2023-05-16')
-        hardDeadlineAt('2023-06-01') // ???
+        softDeadlineAt '2023-05-16'
+        hardDeadlineAt '2023-06-01' // ???
     }
 }
 
 lessons {
     // 1st semester
-    lesson('2022-09-05')
-    lesson('2022-09-12')
-    lesson('2022-09-19')
-    lesson('2022-09-26')
-    lesson('2022-10-03')
-    lesson('2022-10-10')
-    lesson('2022-10-17')
-    lesson('2022-10-24')
-    lesson('2022-11-01')
-    lesson('2022-11-08')
-    lesson('2022-11-15')
-    lesson('2022-11-22')
-    lesson('2022-11-29')
-    lesson('2022-12-06')
-    lesson('2022-12-13')
-    lesson('2022-12-20')
-    lesson('2022-12-27')
+    lessonAt '2022-09-05'
+    lessonAt '2022-09-12'
+    lessonAt '2022-09-19'
+    lessonAt '2022-09-26'
+    lessonAt '2022-10-03'
+    lessonAt '2022-10-10'
+    lessonAt '2022-10-17'
+    lessonAt '2022-10-24'
+    lessonAt '2022-11-01'
+    lessonAt '2022-11-08'
+    lessonAt '2022-11-15'
+    lessonAt '2022-11-22'
+    lessonAt '2022-11-29'
+    lessonAt '2022-12-06'
+    lessonAt '2022-12-13'
+    lessonAt '2022-12-20'
+    lessonAt '2022-12-27'
 
     // 2nd semester
-    lesson('2023-02-07')
-    lesson('2023-02-14')
-    lesson('2023-02-21')
-    lesson('2023-02-28')
-    lesson('2023-03-07')
-    lesson('2023-03-14')
-    lesson('2023-03-21')
-    lesson('2023-03-28')
-    lesson('2023-04-04')
-    lesson('2023-04-11')
-    lesson('2023-04-18')
-    lesson('2023-04-25')
-    lesson('2023-05-02')
-    lesson('2023-05-16')
-    lesson('2023-05-23')
-    lesson('2023-05-30')
+    lessonAt '2023-02-07'
+    lessonAt '2023-02-14'
+    lessonAt '2023-02-21'
+    lessonAt '2023-02-28'
+    lessonAt '2023-03-07'
+    lessonAt '2023-03-14'
+    lessonAt '2023-03-21'
+    lessonAt '2023-03-28'
+    lessonAt '2023-04-04'
+    lessonAt '2023-04-11'
+    lessonAt '2023-04-18'
+    lessonAt '2023-04-25'
+    lessonAt '2023-05-02'
+    lessonAt '2023-05-16'
+    lessonAt '2023-05-23'
+    lessonAt '2023-05-30'
 }
